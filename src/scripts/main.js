b569dd8f1beaa78890e54fc2ff904a63c34013f4
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-08-27 19:09:30 +0200

    complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.querySelectorAll('li')];
+const employeesListObject = employeesList.map((employee) => ({
+  name: employee.innerHTML.trim(),
+  position: employee.dataset.position,
+  salary: Number(employee.dataset.salary.replace(/[$,]/g, '')),
+  age: Number(employee.dataset.age),
+})).sort((a, b) => a.salary - b.salary).reverse();
+
+for (let i = 0; i < employeesListObject.length; i++) {
+  employeesList[i].innerHTML = employeesListObject[i].name;
+  employeesList[i].dataset.position = employeesListObject[i].position;
+  employeesList[i].dataset.salary = employeesListObject[i].salary;
+  employeesList[i].dataset.age = employeesListObject[i].age;
+}
